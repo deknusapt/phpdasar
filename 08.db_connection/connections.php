23d6b2fdf2fc633966d8922acdf2e533f1e840db
deknusapt <dev.deknusa@gmail.com>
2024-11-04 09:01:32 +0800

    Update: Database Connections

---
+++ 08.db_connection/connections.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Variabel untuk kredensial database
+$hostname = "localhost";
+$username = "phpmyadmin_user";
+$password = "myadminpass";
+$dbname = "phpdasar";
+
+// Koneksi ke database
+$conn = mysqli_connect("$hostname","$username","$password","$dbname");
+// Perkondisia memeriksa apakah koneksi ke database berhasil
+if (!$conn){
+    die("Connection failed! " . mysqli_connect_error());
+}
+
+// Fungsi kueri data dari DB ke dalam array
+function querry($querry_syntax)
+{
+    global $conn; // Inisiasi tipe variable untuk mengambil nilai dari luar fungsi
+    $result = mysqli_query($conn, $querry_syntax); // Hasil sintaks kueri dari $conn, disimpan ke variable $result
+    $rows = []; // Array untuk menampung elemen data hasil kueri
+
+    // Ketika data di fetch, ditampung ke variable $row satu per-satu
+    while ($row = mysqli_fetch_assoc($result)){
+        $rows[] = $row; // Kemudian per-elemen data, disatukan menjadi array associatif
+    }
+return $rows;
+}
+?>
---
+++ 08.db_connection/index.php
@@ -0,0 +1,45 @@
+<?php
+require 'connections.php';
+
+// Variable $data menampung hasil kueri dari fungsi querry yang memiliki argumen sintask SQL
+$data = querry("SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Halaman Admin</title>
+</head>
+<body>
+
+<h1>Daftar Mahasiswa</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No.</th>
+        <th>Nama</th>
+        <th>NIM</th>
+        <th>Jurusan</th>
+        <th>eMail</th>
+        <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php foreach ($data as $row): ?>
+    <tr>
+        <td><?= $i ?></td>
+        <td>
+            <a href="">Ubah</a> |
+            <a href="">Aksi</a>
+        </td>
+        <td><?= $row["nama"]?></td>
+        <td><?= $row["nim"]?></td>
+        <td><?= $row["jurusan"]?></td>
+        <td><?= $row["email"]?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+</table>
+
+</body>
+</html>
